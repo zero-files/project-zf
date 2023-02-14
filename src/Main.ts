db03089c1df9b7d64b0d9c32112e51aea2f8a41e
Lottie <enzodiazdev@gmail.com>
2023-02-13 21:13:03 -0300

    User authentication (#4)
    * Message is now a builder
    
    * command handling base
    
    * commands concept
    
    * add commands method
    
    * validate base
    
    ---------
    
    Co-authored-by: Enzo Diaz <enzo.diaz@modo.com.ar>

--- src/Main.ts
+++ src/Main.ts
@@ -1,10 +1,21 @@
 if(process.env.NODE_ENV !== "production") require("dotenv").config();
+import LoginCommand from "game/commands/LoginCommand";
+import RegisterCommand from "game/commands/RegisterCommand";
+import GameInput from "game/GameInput";
 import Application from "./app/Application";
 import Space from "./app/components/Space";
 class Main {
   public static main():void {
     const app = new Application();
-    app.addSpace(new Space("/*"));
+
+    const space = new Space("/*");
+    const gameInput = new GameInput();
+    gameInput.addCommand(new RegisterCommand())
+      .addCommand(new LoginCommand());
+
+    space.messageHandler = gameInput;
+
+    app.addSpace(space);
     app.listen(3000);
   }
 }
